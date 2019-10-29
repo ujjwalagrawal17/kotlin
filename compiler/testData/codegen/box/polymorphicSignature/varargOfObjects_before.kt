c1aa561bd945faaa8fe41de1b6d3753ff3a40ee2
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2019-10-29 15:32:40 +0100

    Support JVM polymorphic signature calls to methods with void return type
     #KT-32026 Fixed

--- compiler/testData/codegen/box/polymorphicSignature/varargOfObjects_before.kt
+++ compiler/testData/codegen/box/polymorphicSignature/varargOfObjects_before.kt
@@ -1,3 +1,4 @@
+// !LANGUAGE: -PolymorphicSignature
 // TARGET_BACKEND: JVM
 // FULL_JDK
 // SKIP_JDK6
@@ -42,6 +43,12 @@ fun box(): String {
 
     val r8 = mh.invoke(arrayOf(args))
     if (r8 !is Array<*> || r8[0] !is Array<*> || !(r8[0] as Array<*>).contentEquals(args)) return "Fail 8: $r8"
+
+    // The next two calls check behavior in a statement context (where the call result is not used)
+
+    mh.invokeExact(args)
+
+    mh.invoke(args)
 
     return "OK"
 }
