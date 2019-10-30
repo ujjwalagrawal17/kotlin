2427398c7718091932c6866319d8fa31fa25ac9a
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2019-10-30 17:43:53 +0300

    [Gradle, JS] Remove explicit extensions on nodejs and yarn

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/Kotlin2JsGradlePluginIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/Kotlin2JsGradlePluginIT.kt
@@ -433,7 +433,19 @@ class Kotlin2JsGradlePluginIT : BaseGradleIT() {
             )
         }
 
-        gradleBuildScript().appendText("\nyarn.version = \"1.9.3\"")
+        fun setYarnVersion(version: String): String {
+            return """
+            plugins.withType<YarnPlugin> {
+                (extensions[YarnRootExtension.YARN] as YarnRootExtension).apply {
+                    version = "$version"
+                }
+            }
+            """
+        }
+
+        gradleBuildScript().appendText(
+            setYarnVersion("1.9.3")
+        )
 
         build("yarnConcreteVersionFolderChecker") {
             assertSuccessful()
@@ -444,13 +456,17 @@ class Kotlin2JsGradlePluginIT : BaseGradleIT() {
             )
         }
 
-        gradleBuildScript().appendText("\nyarn.version = \"1.9.2\"")
+        gradleBuildScript().appendText(
+            setYarnVersion("1.9.2")
+        )
 
         build("kotlinYarnSetup") {
             assertSuccessful()
         }
 
-        gradleBuildScript().appendText("\nyarn.version = \"1.9.3\"")
+        gradleBuildScript().appendText(
+            setYarnVersion("1.9.3")
+        )
 
         build("kotlinYarnSetup") {
             assertSuccessful()
